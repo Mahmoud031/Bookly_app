3074e225f406cd7ab6b32bf05ca5226bb811e08d
Mahmoud031 <mahmoudrady007@gmail.com>
2025-02-04 20:37:27 +0200

    add custom list view item

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/utils/assets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+      child: Row(
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 24,
+          ),
+          const Spacer(),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                size: 24,
+              ))
+        ],
+      ),
+    );
+  }
+}
--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/constants.dart';
 import 'package:flutter_application_1/features/splash/presentation/views/widgets/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
