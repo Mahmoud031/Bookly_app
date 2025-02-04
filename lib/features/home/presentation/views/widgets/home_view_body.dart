0676e5f1e9891fb03747551ca8db1b7eecf5d162
Mahmoud031 <mahmoudrady007@gmail.com>
2025-02-04 21:15:28 +0200

    create styles file & add medium title home view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/utils/styles.dart';
 
 import 'custom_app_bar.dart';
-import 'custom_list_view_item.dart';
+import 'featured_books_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -9,28 +10,23 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [CustomAppBar(), FeaturedListView()],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomAppBar(),
+            FeaturedBooksListView(),
+            const SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Best Seller",
+              style: Styles.titleMedium,
+            )
+          ],
+        ),
       ),
-    );
-  }
-}
-
-class FeaturedListView extends StatelessWidget {
-  const FeaturedListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 6.0),
-              child: CustomListViewItem(),
-            );
-          }),
     );
   }
 }
