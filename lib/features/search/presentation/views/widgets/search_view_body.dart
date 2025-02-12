214eb646a54eb76901a210d8b19cb9e85d491784
Mahmoud031 <mahmoudrady007@gmail.com>
2025-02-12 17:49:25 +0200

    search view done

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_application_1/core/utils/styles.dart';
+import 'package:flutter_application_1/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 
 import 'custom_search_text_field.dart';
 
@@ -9,14 +10,40 @@ class SearchViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 14),
       child: Column(
-        children: [CustomSearchTextField()],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomSearchTextField(),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            "search Results",
+            style: Styles.textStyle18,
+          ),
+          Expanded(child: SearchResultListView())
+        ],
       ),
     );
   }
 }
 
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
 
-
-  
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      shrinkWrap: true, // Ensure the ListView takes only the space it needs
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: BookListViewItem(),
+        );
+      },
+    );
+  }
+}
