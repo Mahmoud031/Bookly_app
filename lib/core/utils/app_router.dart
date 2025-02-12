fc2308560ca56c416c5020a9957e0443e9fe1dd0
Mahmoud031 <mahmoudrady007@gmail.com>
2025-02-12 17:29:42 +0200

    creates search view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,15 +1,21 @@
 import 'package:flutter_application_1/features/home/presentation/views/book_details_view.dart';
 import 'package:flutter_application_1/features/home/presentation/views/home_view.dart';
+import 'package:flutter_application_1/features/search/presentation/views/search_view.dart';
 import 'package:flutter_application_1/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBookDetailsView = '/bookDetailsView';
+  static const kSearchView = '/searchView';
   static final router = GoRouter(routes: [
     GoRoute(
       path: '/',  // the start page
       builder: (context, state) => const SplashView(),
+    ),
+    GoRoute(
+      path: kSearchView,  // the start page
+      builder: (context, state) => const SearchView(),
     ),
     GoRoute(
       path: kHomeView,
