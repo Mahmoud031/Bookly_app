7b3a469b47bfba9b484c20345712bf350c4f98a9
Mahmoud031 <mahmoudrady007@gmail.com>
2025-02-24 22:28:39 +0200

    create home repo

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,6 @@
+
+//repostiorty pattern : create an abstract class for the repository and specify the features that will implement without how to implement it
+abstract class HomeRepo {
+  fetcBestSellerBooks();
+  fetcFeaturedBooks();
+}
