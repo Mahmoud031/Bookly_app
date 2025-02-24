2b437fb34dab102f427f449b216e8d8a100dedcf
Mahmoud031 <mahmoudrady007@gmail.com>
2025-02-24 22:40:51 +0200

    create home repo methods

---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,6 @@
+abstract class Failures {
+
+}
+
+class ServerFailure extends Failures {
+}
--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,6 +1,12 @@
 
 //repostiorty pattern : create an abstract class for the repository and specify the features that will implement without how to implement it
+import 'package:dartz/dartz.dart';
+import 'package:flutter_application_1/core/errors/failures.dart';
+
+import '../models/book_model/book_model.dart';
+
 abstract class HomeRepo {
-  fetcBestSellerBooks();
-  fetcFeaturedBooks();
+ Future<Either<Failures,List<BookModel>>> fetchBestSellerBooks(
+ );
+  Future<Either<Failures,List<BookModel>>>fetchFeaturedBooks();
 }
