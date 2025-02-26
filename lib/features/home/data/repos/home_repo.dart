a2ae5faefc3f1db8bbcda4cd498bdd3fb01cb8c3
Mahmoud031 <mahmoudrady007@gmail.com>
2025-02-26 14:22:51 +0200

    implement fetch newest books

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,3 @@
-
 //repostiorty pattern : create an abstract class for the repository and specify the features that will implement without how to implement it
 import 'package:dartz/dartz.dart';
 import 'package:flutter_application_1/core/errors/failures.dart';
@@ -6,7 +5,6 @@ import 'package:flutter_application_1/core/errors/failures.dart';
 import '../models/book_model/book_model.dart';
 
 abstract class HomeRepo {
- Future<Either<Failures,List<BookModel>>> fetchBestSellerBooks(
- );
-  Future<Either<Failures,List<BookModel>>>fetchFeaturedBooks();
+  Future<Either<Failures, List<BookModel>>> fetchNewestBooks();
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks();
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,12 +1,27 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_application_1/core/errors/failures.dart';
+import 'package:flutter_application_1/core/utils/api_service.dart';
 import 'package:flutter_application_1/features/home/data/models/book_model/book_model.dart';
 import 'package:flutter_application_1/features/home/data/repos/home_repo.dart';
 
-class HomeRepoImpl implements HomeRepo{
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() {
-    
+  Future<Either<Failures, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
